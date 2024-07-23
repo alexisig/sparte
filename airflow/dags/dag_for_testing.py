c3289324db2a879550a9119414989769fa835caf
Alexis A. <alexis.athlani@gmail.com>
2024-07-23 14:39:19 +0200

    temp

---
+++ airflow/dags/dag_for_testing.py
@@ -0,0 +1,26 @@
+import os
+
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.python_operator import PythonOperator
+
+from airflow import DAG
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+
+def my_callable(*args, **kwargs):
+    from geopandas import show_versions
+
+    show_versions()
+
+
+with DAG(
+    "dag_for_testing",
+    schedule_interval="@once",
+) as dag:
+    download_ocsge_7z_file_task = EmptyOperator(task_id="download_ocsge_7z_file")
+    unzip_ocsge_7z_to_shapefile_task = EmptyOperator(task_id="unzip_ocsge_7z_file")
+    create_geopackage_from_ocsge_shapefile = EmptyOperator(task_id="repack_ocsge_shapefile_to_geopackage")
+    add_additionnal_fields_to_geopackage_task = EmptyOperator(task_id="add_additionnal_fields_to_geopackage")
+    add_artficial_area_layer_to_geopackage = EmptyOperator(task_id="calculate_artficial_area")
+    test_operator = PythonOperator(task_id="test_operator", python_callable=my_callable)
